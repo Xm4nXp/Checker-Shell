9076f1b6c7ddde618eb2de216e3f89908ffe1da3
Maman <85057664+Xm4nXp@users.noreply.github.com>
2022-09-03 19:06:50 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+apt update; apt upgrade
+sleep 2
+clear
+echo ""
+echo -e "\e[32mInstalling Module..."
+echo -e "\e[37m"
+pip install rich
+pip install futures
+pip install requests
+pip install bs4
+pip install colorama
+pip install pyfiglet
+pip install tqdm
+echo ""
+echo -e "\e[32m[*] Done! All Module Has Been Installed!"
+sleep 2
+echo "Wait 5s! Closing..."
+sleep 5
+clear
